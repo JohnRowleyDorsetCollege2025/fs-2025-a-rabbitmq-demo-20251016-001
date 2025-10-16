73f0f2f0eaefabdf7f1335881e53ac1548188b98
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-10-16 12:03:56 +0100

    added consumer project to rabbit mq

--- RabbitMQDemo002/Program.cs
+++ RabbitMQDemo002/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//var uri = Environment.GetEnvironmentVariable("CLOUDAMQP_URL")
+//    ?? throw new InvalidOperationException("Missing CLOUDAMQP_URL environment variable.");
+var uri = "amqps://ugvmwgak:Jk99nW7GcnUP46OdrD4TqW0kYKalVQyB@ostrich.lmq.cloudamqp.com/ugvmwgak";
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri(uri),
+    DispatchConsumersAsync = true
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+const string queue = "demo-queue";
+channel.QueueDeclare(queue, durable: true, exclusive: false, autoDelete: false);
+
+Console.WriteLine($"[Consumer] Connected to CloudAMQP. Waiting for messages in '{queue}'...");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.Received += async (_, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine($"[Consumer] Received: {message}");
+    await Task.Delay(500); // simulate processing
+    channel.BasicAck(ea.DeliveryTag, false);
+};
+
+channel.BasicConsume(queue, autoAck: false, consumer);
+await Task.Delay(Timeout.Infinite);
+
